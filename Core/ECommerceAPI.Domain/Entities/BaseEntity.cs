e94a7156ba1184e2a001e2820bd42eeeddfd1b6e
Oğuzhan Kumsal <oguzhan.kumsal@stoneity.com>
2025-01-22 16:24:11 +0300

    save changes inteceptor

--- Core/ECommerceAPI.Domain/Entities/BaseEntity.cs
+++ Core/ECommerceAPI.Domain/Entities/BaseEntity.cs
@@ -10,5 +10,6 @@ namespace ECommerceAPI.Domain.Entities
     {
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
     }
 }
--- Infrastructure/ECommerceAPI.Persistance/Contexts/ECommerceAPIDbContext.cs
+++ Infrastructure/ECommerceAPI.Persistance/Contexts/ECommerceAPIDbContext.cs
@@ -17,5 +17,19 @@ namespace ECommerceAPI.Persistance.Contexts
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var data = ChangeTracker.Entries().Where(data => data.State == EntityState.Added || data.State == EntityState.Modified);
+            foreach (var item in data)
+            {
+                if (item.State == EntityState.Added)
+                {
+                    item.CurrentValues["CreatedDate"] = DateTime.Now;
+                }
+                item.CurrentValues["UpdatedDate"] = DateTime.Now;
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
